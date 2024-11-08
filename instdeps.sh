632c084cca170635f780c3fc87432e80d377c1b2
Markus Iser <markus.iser@kit.edu>
2024-11-08 10:27:17 +0100

    asdf

--- instdeps.sh
+++ instdeps.sh
@@ -4,17 +4,18 @@ if command -v dnf > /dev/null; then
     dnf install -y libarchive-devel
     dnf install -y cmake
     dnf groupinstall -y "Development Tools"
+    git clone https://github.com/pybind/pybind11.git
+    cd pybind11
+    mkdir build
+    cd build
+    cmake ..
+    make -j6 install
 elif command -v apt > /dev/null; then
     sudo apt update
     sudo apt install -y libarchive-dev
     sudo apt install -y cmake
     sudo apt install -y build-essential
+    sudo apt install -y pybind11-dev
 fi
+
 pip install scikit-build
-# cd /project
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-mkdir build
-cd build
-cmake ..
-make -j6 install

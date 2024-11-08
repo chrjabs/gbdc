a32951dc2e88fb95b0e85f66bbfd3427983cec2f
Markus Iser <markus.iser@kit.edu>
2024-11-08 11:07:08 +0100

    asdf

--- instdeps.sh
+++ instdeps.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-if command -v dnf > /dev/null; then
-    dnf install -y libarchive-devel
-    dnf install -y cmake
-    dnf groupinstall -y "Development Tools"
+install_pybind_devel_from_source() {
     git clone https://github.com/pybind/pybind11.git
     cd pybind11
     mkdir build
     cd build
     cmake ..
     make -j6 install
+}
+
+if command -v dnf > /dev/null; then
+    dnf install -y libarchive-devel
+    dnf install -y cmake
+    dnf groupinstall -y "Development Tools"
+    install_pybind_devel_from_source
 elif command -v yum > /dev/null; then
     yum install -y libarchive-devel
     yum install -y cmake
     yum groupinstall -y "Development Tools"
-    yum install -y pybind11-dev
+    install_pybind_devel_from_source
 else
     apt update
     apt install -y libarchive-dev
